1cfbd46fab4c20ba6469e4ec232aa24fa84711d1
yogurt1989 <mauropappaterra@gmail.com>
2018-03-30 15:57:42 +0200

    Corrected csv2json.js, deleted unecessary files

--- CVS to JSON Converter/csv2json.js
+++ CVS to JSON Converter/csv2json.js
@@ -7,36 +7,47 @@
 const fs = require('fs');  // file system module
 const readline = require('readline'); // read line module
 
-const csv2json = (csv_file = 'customer-data.csv') => {
+const csv2json = (file = 'customer-data') => {
 
     var all_customers = [];
+    var keys = [];
+    var no_keys = 0;
+    var counter = 0;
 
     var lineReader = readline.createInterface({
-        input: fs.createReadStream(csv_file)
+        input: fs.createReadStream(file + '.csv')
     });
 
-    lineReader.on('line', function (line) {
-        //console.log('\ncustomer: ', line);
-        var temp = line.split(',');
-        var customer = {
-            'id': temp[0],
-            'first_name': temp[1],
-            'last_name': temp[2],
-            'email': temp[3],
-            'gender': temp[4],
-            'ip_address': temp[5],
-            'ssn': temp[6],
-            'credit_card': temp[7],
-            'bitcoin': temp[8],
-            'street_address': temp[9]
-        };
-        //console.log(temp)
-        //console.log(customer)
-        all_customers.push(customer);
-    });
+    lineReader.on('line', (line, callback) => {
 
-    console.log(all_customers);
+        if (counter == 0){
+            keys = line.split(',') // get the keys from the first line
+            no_keys = keys.length
+            //console.log('\nKeys: ', keys);
+
+        } else {
+            var customer_info = line.split(',')
+            //console.log(customer_info)
+
+            var customer = new Object()
+
+            for (var i = 0; i <no_keys; i++){
+                customer[keys[i]] = customer_info[i]
+            }
+            //console.log(customer)
+            all_customers.push(customer);
+        }
+
+        counter ++;
+    })
+
+    lineReader.on('close',() => {
+        fs.writeFile(file + '.json', JSON.stringify(all_customers,null,' '), (error) => {
+            if (error)
+                return process.exit(1)
+    });
+        console.log("CSV file converted to .JSON successfully")
+    })
 }
 
 csv2json(process.argv[2]);
-
